325520a494449dd677c173caa0743e665f9a36ba
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-05-04 22:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 const makeChange = (c) => {
   // your name here
+	const amount = parseInt(c);
+	const values = {
+		q:25,
+		d:10,
+		n:5,
+		p=1
+	}
+
+	let quarters=0, dimes=0, nickels=0, pennies=0;
+
+	while(c > 0){
+		if(c >= values.q){
+			quarters++;
+			c-=values.q;
+		}
+
+		if(c >= values.d){
+			dimes++;
+			c-=values.d;
+		}
+
+		if(c >= values.n){
+			nickels++;
+			c-=values.n;
+		}
+ 
+		if(c >= values.p){
+			pennies++;
+			c-=values.p;
+		}
+	}
+
+	return {
+		q : quarters,
+		d : dimes,
+		n : nickels,
+		p : pennies
+	};
 };
 
 // Do not the change the code below
