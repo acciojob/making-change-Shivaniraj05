1996c25dca0d97e9894f7744977d6b8147612e41
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-05-04 22:24:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,28 +5,29 @@ const makeChange = (c) => {
 		q:25,
 		d:10,
 		n:5,
-		p=1
-	}
+		p:1
+	}; 
 
 	let quarters=0, dimes=0, nickels=0, pennies=0;
 
 	while(c > 0){
+		
 		if(c >= values.q){
 			quarters++;
 			c-=values.q;
 		}
 
-		if(c >= values.d){
+		else if(c >= values.d){
 			dimes++;
 			c-=values.d;
 		}
 
-		if(c >= values.n){
+		else if(c >= values.n){
 			nickels++;
 			c-=values.n;
 		}
  
-		if(c >= values.p){
+		else if(c >= values.p){
 			pennies++;
 			c-=values.p;
 		}
